d265f2e9960a81fc3520d1f9b9dca848c86e3fa6
SirDavid07 <okafordavid0207@gmail.com>
2024-10-13 17:07:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    document.getElementById("send").onclick=function()
+}
+var name = document.getElementById("name").value 
+var password= document. getElementById("pass"). value, if (name ==""){
+    name= "NULL"
+}
+if (password ==""){
+    'password' = "NULL"
+}
